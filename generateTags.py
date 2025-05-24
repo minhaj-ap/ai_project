445855e788468e7ba015ebd6737d120aaf07bfe4
Minhaj Ap <minhajap00@gmail.com>
2025-05-24 08:45:24 +0530

    Refactor tag generation logic to use set for tags and remove unused is_tag_worthy function

--- generateTags.py
+++ generateTags.py
@@ -1,40 +1,29 @@
 import spacy
-from scipy.spatial.distance import cosine
 
 # NOTE:tags shouldn't be one word unless it convey porper meaning
-# Load the small English pipeline
+# Loading the large English pipeline
 nlp = spacy.load("en_core_web_lg")
-
-
-def is_tag_worthy(word: str, threshold=0.5) -> bool:
-    if word not in nlp.vocab and not nlp(word).has_vector:
-        print(f"{word} is not a word or doesnt have vector")
-        return False  # returning if no vector exists or if the word itself exists
-    similarity_count = 0  # setting count to check how not generic a word is
-    for Word in nlp.vocab.strings:
-        if Word == word:
-            continue  # skipping the word itself to optimize function from unwanted calles
-        similarity = 1 - cosine(
-            nlp.vocab[word].vector, nlp.vocab[Word].vector
-        )  # checking genratily distance using cosine distance
-        # to know how generic a word is
-        if similarity > threshold:
-            similarity_count += 1  # considering the word only if it has a similarity more than threshold (0.5)
-    print(similarity_count)
-    return (
-        similarity_count > 150
-    )  # choosing words only with higher similarity count, more higher more tag worthy
 
 
 def generateTags(paragraph: str) -> list:
     doc = nlp(paragraph)
     # Iterate over the tokens
-    tags = []
+    tags = set()
+    for ent in doc.ents:
+        if ent.label_ in ["PERSON", "ORG", "EVENT"]:
+            tags.add(ent.text.strip().lower())
     for token in doc:
-        print(f"{token.text} {token.pos_} {token.head.text}")
-        if token.pos_ == "ADJ" and is_tag_worthy(token.text):
-            tags.append(f"{token.text}")
-        elif token.pos_ == "NOUN" and is_tag_worthy(token.text):
-            tags.append(f"{token.text}")
-    print(tags)
-    return tags
+        if any(token.text in tag.split() for tag in tags):
+            continue
+        elif token.pos_ == "ADJ":
+            tags.add(
+                f"{token.lemma_.lower()} {token.head.text.lower() if token.head.pos_ =='NOUN' else ''}"
+            )
+        elif token.pos_ == "NOUN" and token.head.pos_ == "VERB":
+            tags.add(f"{token.lemma_.lower()} {token.head.text}")
+    for chunk in doc.noun_chunks:
+        if any(word in tag.split() for tag in tags for word in chunk.text.split()):
+            continue
+        elif len(chunk.text.strip().lower()) > 1:
+            tags.add(chunk.text.strip().lower())
+    return list(tags)
